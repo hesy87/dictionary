53f8bbb748f3ae4d2e4fa450976e60abf17489a9
Hesam Mahmoodi <hesam.mahmoodi@googlemail.com>
2022-08-27 16:55:25 +0430

    import json

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
-let words = [
-  {
-    en: "apple",
-    per: "سیب",
-    sample: "This is an apple",
-  },
-  {
-    en: "orange",
-    per: "پرتقال",
-    sample: "This is an orange",
-  },
-  {
-    en: "hello",
-    per: "سلام",
-    sample: "hello I'm hesam",
-  },
-  {
-    en: "pen",
-    per: "خودکار",
-    sample: "you can write whit it",
-  },
-];
+// let words = [
+//   {
+//     en: "apple",
+//     per: "سیب",
+//     sample: "This is an apple",
+//   },
+//   {
+//     en: "orange",
+//     per: "پرتقال",
+//     sample: "This is an orange",
+//   },
+//   {
+//     en: "hello",
+//     per: "سلام",
+//     sample: "hello I'm hesam",
+//   },
+//   {
+//     en: "pen",
+//     per: "خودکار",
+//     sample: "you can write whit it",
+//   },
+// ];
 
+
+import words from "./words.json" ;
 function search() {
   let word = document.querySelector("#inputWord").value;
   document.querySelector(".searchInput").innerText = word;
-  for (let i = 0; i < words.length; i++)
-    if (word == words[i].en) {
-      document.querySelector(".answer").innerText = words[i].per;
-      document.querySelector(".sample").innerText = words[i].sample;
-    }
+  console.log(words.words.en)
+  // for (let i = 0; i < words.words.length; i++)
+  //   if (word == words.words[0].en) {
+  //     document.querySelector(".answer").innerText = words.words[1].per;
+  //     document.querySelector(".sample").innerText = words.words[2].sample;
+  //   }
 }
