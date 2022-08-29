328d7569da6fbea797165e881e95100e8c30ed1c
hesam.mahmoodi <hesam.mahmoodi@gmail.com>
2022-08-29 15:31:13 +0430

    accordian developed

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-
-let thisWordIsCorrect = null; 
-fetch('./words.json')
-  .then(response => {
+let thisWordIsCorrect = null;
+fetch("./words.json")
+  .then((response) => {
     return response.json();
   })
-  .then(jsondata => {
-    this.thisWordIsCorrect = jsondata.words
+  .then((jsondata) => {
+    this.thisWordIsCorrect = jsondata.words;
   });
 function search() {
   let word = document.querySelector("#inputWord").value;
@@ -15,4 +14,45 @@ function search() {
   });
   document.querySelector(".answer").innerText = answer[0].per;
   document.querySelector(".sample").innerText = answer[0].sample;
+}
+
+
+let acc = true
+function openAcc() {
+  if (acc) {
+    document.querySelector(".searchInput").style.display = "block";
+    document.querySelector(".caret").style.transform = "rotate(180deg)";
+    acc = false
+  }
+  else {
+    document.querySelector(".searchInput").style.display = "none";
+    document.querySelector(".caret").style.transform = "rotate(0)";
+    acc = true
+  }
+}
+
+function openAcc2() {
+  if (acc) {
+    document.querySelector(".answer").style.display = "block";
+    document.querySelector(".caret2").style.transform = "rotate(180deg)";
+    acc = false
+  }
+  else {
+    document.querySelector(".answer").style.display = "none";
+    document.querySelector(".caret2").style.transform = "rotate(0)";
+    acc = true
+  }
+}
+
+function openAcc3() {
+  if (acc) {
+    document.querySelector(".sample").style.display = "block";
+    document.querySelector(".caret3").style.transform = "rotate(180deg)";
+    acc = false
+  }
+  else {
+    document.querySelector(".sample").style.display = "none";
+    document.querySelector(".caret3").style.transform = "rotate(0)";
+    acc = true
+  }
 }
